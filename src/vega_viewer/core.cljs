607bcef7e68aea411176c4abab89de726f1341d2
Okal Otieno <okal@justokal.com>
2016-01-13 17:26:54 +0300

    OO: Add rudimentary vega-viewer component

--- src/vega_viewer/core.cljs
+++ src/vega_viewer/core.cljs
@@ -1,15 +1,71 @@
 (ns vega-viewer.core
   (:require
-   #_[om.core :as om :include-macros true]
+   [cljsjs.vega]
+   [om.core :as om :include-macros true]
    [sablono.core :as sab :include-macros true])
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest]]))
 
 (enable-console-print!)
 
-(defcard first-card
-  (sab/html [:div
-             [:h1 "This is your first devcard!"]]))
+(def vega-spec
+  (atom {:width 200
+         :height 200
+         :data [{:name "fruits"
+                 :values [{"name" "apple"
+                           "number" 23}
+                          {"name" "oranges"
+                           "number" 42}]}]
+         :scales [{:name "category"
+                   :type "ordinal"
+                   :domain {:data "fruits" :field "name"}
+                   :range "width"}
+                  {:name "frequency"
+                   :type "linear"
+                   :range "height"
+                   :domain {:data "fruits" :field "number"}}]
+         :axes [{:scale "frequency" :type "y"}
+                {:scale "category" :type "x"}]
+         :marks [{:from {:data "fruits"}
+                  :type "rect"
+                  :properties {:enter {:x {:scale "category" :field "name"}
+                                       :width {:scale "category" :band true :offset -1}
+                                       :y {:scale "frequency" :field "number"}
+                                       :y2 {:scale "frequency" :value 0}}
+                               :update {:fill {:value "steelblue"}}}}]}))
+
+(defn vega-viewer
+  [vega-spec owner]
+  (reify
+    om/IDidUpdate
+    (did-update [_ _ _]
+      (let [vega-container (om/get-node owner "vega-container")
+            vega-spec-as-js (clj->js @vega-spec)]
+        (js/console.log vega-spec-as-js)
+        (js/vg.parse.spec vega-spec-as-js
+                          (fn [chart]
+                            (js/console.log chart)
+                            (let [view (chart #js {:el vega-container})]
+                              (.update view))))))
+    om/IDidMount
+    (did-mount [_]
+      (let [vega-container (om/get-node owner "vega-container")
+            vega-spec-as-js (clj->js @vega-spec)]
+        (js/console.log vega-spec-as-js)
+        (js/vg.parse.spec vega-spec-as-js
+                          (fn [chart]
+                            (js/console.log chart)
+                            (let [view (chart #js {:el vega-container})]
+                              (.update view))))))
+    om/IRender
+    (render
+        [_]
+      (sab/html
+       [:div [:h1 "Vega Viewer"]
+        [:div {:ref "vega-container"}]]))))
+
+(defcard vega-chart
+  (om/build vega-viewer vega-spec))
 
 (defn main []
   ;; conditionally start the app based on wether the #main-app-area
