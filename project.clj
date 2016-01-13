2c6bcb4ace59d520191975a6617a471676fb1cda
Okal Otieno <okal@justokal.com>
2016-01-13 16:21:53 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject vega-viewer "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [devcards "0.2.1"]
+                 [sablono "0.4.0"]
+                 #_[org.omcljs/om "0.9.0"]
+                 #_[reagent "0.5.1"]]
+
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-figwheel "0.5.0-1"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+  
+  :source-paths ["src"]
+
+  :cljsbuild {
+              :builds [{:id "devcards"
+                        :source-paths ["src"]
+                        :figwheel { :devcards true } ;; <- note this
+                        :compiler { :main       "vega-viewer.core"
+                                    :asset-path "js/compiled/devcards_out"
+                                    :output-to  "resources/public/js/compiled/vega_viewer_devcards.js"
+                                    :output-dir "resources/public/js/compiled/devcards_out"
+                                    :source-map-timestamp true }}
+                       {:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main       "vega-viewer.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/vega_viewer.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main       "vega-viewer.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/vega_viewer.js"
+                                   :optimizations :advanced}}]}
+
+  :figwheel { :css-dirs ["resources/public/css"] })
