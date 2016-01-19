15f1f896c49f4853de1a42882fdb2d16d6d8dff5
Okal Otieno <okal@justokal.com>
2016-01-19 17:16:37 +0300

    OO: Add bogus placeholder test Need to figure out how to include a json-schema validator

--- test/vega_viewer/vega/specs_test.cljs
+++ test/vega_viewer/vega/specs_test.cljs
@@ -1,2 +1,14 @@
-(ns vega-viewer.vega.specs-test)
+(ns vega-viewer.vega.specs-test
+  (:require-macros [vega-viewer.macros :refer [read-file]])
+  (:require [cljs.test :refer-macros [deftest is]]
+            [cljsjs.vega]
+            [cljsjs.llexus-validate]
+            [vega-viewer.vega.specs
+             :refer [generate-horizontal-bar-chart-vega-spec]]))
 
+(deftest spec-is-vega-compliant
+  (let [data [{"category" "something" "frequency" 2}
+              {"category" "something-else" "frequency" 3}]
+        spec (generate-horizontal-bar-chart-vega-spec data)
+        vega-schema (read-file "test/fixtures/vega-schema.json")]
+    (is true)))
