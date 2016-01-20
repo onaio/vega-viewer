643706193f48f7e6276c91a54f003dcd1be7302e
Okal Otieno <okal@justokal.com>
2016-01-20 13:45:10 +0300

    OO: [WIP] Check generated spec against Vega JSON schema

--- test/vega_viewer/vega/specs_test.cljs
+++ test/vega_viewer/vega/specs_test.cljs
@@ -1,8 +1,7 @@
 (ns vega-viewer.vega.specs-test
   (:require-macros [vega-viewer.macros :refer [read-file]])
   (:require [cljs.test :refer-macros [deftest is]]
-            [cljsjs.vega]
-            [cljsjs.llexus-validate]
+            [cljsjs.tv4]
             [vega-viewer.vega.specs
              :refer [generate-horizontal-bar-chart-vega-spec]]))
 
@@ -10,5 +9,14 @@
   (let [data [{"category" "something" "frequency" 2}
               {"category" "something-else" "frequency" 3}]
         spec (generate-horizontal-bar-chart-vega-spec data)
-        vega-schema (read-file "test/fixtures/vega-schema.json")]
-    (is true)))
+        spec-as-json (clj->js spec)
+        vega-schema (-> "test/fixtures/vega-schema.json"
+                        read-file
+                        js/JSON.parse)
+        valid? (.validate js/tv4 spec vega-schema)
+        error-report (when-not valid?
+                       {:message js/tv4.error.message
+                        :code js/tv4.error.code
+                        :data-path js/tv4.error.dataPath
+                        :spec spec})]
+    (is valid? error-report)))
