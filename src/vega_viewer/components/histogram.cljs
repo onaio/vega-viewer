1f11b9a28481e5a90121e1c81ed114bb13957a0e
Roy Rutto <royrutto@gmail.com>
2016-01-20 17:48:05 +0300

    RR: add histogram spec

---
+++ src/vega_viewer/components/histogram.cljs
@@ -0,0 +1,16 @@
+(ns vega-viewer.components.histogram
+(:require [om.core :as om :include-macros true]
+  [sablono.core :refer-macros [html]]
+  [vega-viewer.components.vega-viewer :refer [vega-viewer]]
+  [vega-viewer.vega.specs
+   :refer [generate-histogram-chart]]))
+
+(defn histogram-chart
+  [{:keys [field_xpath data field_label]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [vega-spec (generate-histogram-chart data field_xpath field_label)
+
+            _ (.log js/console (.stringify js/JSON (clj->js vega-spec)))]
+        (html (om/build vega-viewer vega-spec))))))
--- src/vega_viewer/vega/specs.cljs
+++ src/vega_viewer/vega/specs.cljs
@@ -1,6 +1,8 @@
 (ns vega-viewer.vega.specs)
 
 (def bar-height 31)
+(def histogram-height 200)
+(def default-bin-size 15)
 
 (def vega-spec-template
   {:data [{:name "entries"
@@ -25,6 +27,47 @@
                                  :x2 {:value 0}}
                          :update {:fill {:value "steelblue"}}}}]})
 
+(def histogram-spec-template
+  {:data [{:name "entries"
+           :values []
+           :format {:type "json"
+                    :parse {}}
+           :transform [{:type "bin"
+                        :field ""
+                        :maxbins default-bin-size
+                        :minstep 1}]}
+          {:name "summary"
+           :source "entries"
+           :transform [{:type "aggregate"
+                        :groupby ["bin_start" "bin_end"]
+                        :summarize {:* ["count"]}}]}]
+   :scales [{:name "x"
+             :type "linear"
+             :range "width"
+             :domain {:data "summary" :field ["bin_start" "bin_end"]}}
+            {:name "y"
+             :type "linear"
+             :domain {:data "summary" :field "count" :sort true}
+             :range "height"}]
+   :axes [{:type "x" :scale "x" :ticks 10}
+          {:type "y" :scale "y", :title "Number of Records"}]
+   :marks [{:type "rect"
+            :properties {:enter
+                         {:x {:scale "x" :field "bin_start" :offset 1}
+                          :x2 {:scale "x" :field "bin_end"}
+                          :y {:scale "y" :field "count"}
+                          :y2 {:field {:group "height"}}
+                          :fill {:value "#4682b4"}}}
+            :from {:data "summary"}}
+           {:type "text"
+            :from {:mark "rect"}
+            :properties {:enter {:x {:field "x2"}
+                                 :y {:field "y"}
+                                 :dy {:field "dy"}
+                                 :fill {:value "black"}
+                                 :baseline {:value "bottom"}
+                                 :text {:field "datum.count"}}}}]})
+
 (defn generate-horizontal-bar-chart-vega-spec
   [data]
   (let [tick-count (->> data
@@ -33,4 +76,15 @@
     (-> vega-spec-template
         (assoc-in [:data 0 :values] data)
         (assoc-in [:height] (* (count data) bar-height))
-        (assoc-in [:axes 0 :ticks] tick-count))))
+        (assoc-in [:axes 0 :ticks] 10))))
+
+(defn generate-histogram-chart
+  [data field_xpath field_label]
+  (let [data (apply concat (map (fn [datum] (for [n (range (:count datum))]
+                                              (dissoc datum :count))) data))]
+    (-> histogram-spec-template
+        (assoc-in [:height] histogram-height)
+        (assoc-in [:data 0 :values] data)
+        (assoc-in [:data 0 :format :parse] {field_xpath "number"})
+        (assoc-in [:data 0 :transform 0 :field] field_xpath)
+        (assoc-in [:axes 0 :title] field_label))))
