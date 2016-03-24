ab36dd106e3af27bc57298c5621e91252411379f
Okal Otieno <okal@justokal.com>
2016-03-24 14:40:41 +0300

    OO: Refactor and add docstrings

--- src/vega_viewer/components/vega_viewer.cljs
+++ src/vega_viewer/components/vega_viewer.cljs
@@ -3,18 +3,39 @@
             [om.core :as om :include-macros true]
             [sablono.core :refer-macros [html]]))
 
+(def chart-width-proportion 0.8)
+
 (defn set-resize-handler
+  "Add a window.resize event handler that changes the :container-width state of
+   a vega-viewer instance when the window is resized"
   [owner]
   (let [resize-handler
         (.addEventListener js/window "resize"
                            (fn [event]
-                             (om/set-state! owner
-                                            :container-width
-                                            (- (.-clientWidth (om/get-node owner "vega-container"))
-                                               30))))]
+                             (om/set-state!
+                              owner
+                              :container-width
+                              (.-clientWidth
+                               (om/get-node owner "vega-container")))))]
     (om/set-state! owner :resize-handler resize-handler)))
 
+(defn render-vega-visualization
+  "Render a Vega specification in the supplied container"
+  [spec container responsive?]
+  (let [container-width (.-clientWidth container)
+        spec-as-js (clj->js (if responsive?
+                                   (assoc spec
+                                          :width
+                                          (* container-width
+                                             chart-width-proportion))
+                                   spec))]
+    (js/vg.parse.spec spec-as-js
+                      (fn [chart]
+                        (let [view (chart #js {:el container})]
+                          (.update view))))))
+
 (defn vega-viewer
+  "Return an Om component that renders a Vega specification"
   [{:keys [width] :as vega-spec} owner {:keys [responsive?]}]
   (reify
     om/IWillUnmount
@@ -24,35 +45,19 @@
 
     om/IDidUpdate
     (did-update [_ _ {previous-container-width :container-width}]
-      (let [vega-container (om/get-node owner "vega-container")
-            container-width (.-clientWidth vega-container)
-            vega-spec-as-js (clj->js (if responsive?
-                                       (assoc vega-spec
-                                              :width
-                                              (* container-width
-                                                 0.8))
-                                       vega-spec))]
+      (let [{:keys [container-width]} (om/get-state owner)]
         (when (not= previous-container-width container-width)
-          (js/vg.parse.spec vega-spec-as-js
-                            (fn [chart]
-                              (let [view (chart #js {:el vega-container})]
-                                (.update view)))))))
+          (render-vega-visualization vega-spec
+                                     (om/get-node owner "vega-container")
+                                     responsive?))))
 
     om/IDidMount
     (did-mount [_]
       (set-resize-handler owner)
-      (let [vega-container (om/get-node owner "vega-container")
-            container-width (.-clientWidth vega-container)
-            vega-spec-as-js (clj->js (if responsive?
-                                       (assoc vega-spec
-                                              :width
-                                              (* container-width
-                                                 0.8))
-                                       vega-spec))]
-        (js/vg.parse.spec vega-spec-as-js
-                          (fn [chart]
-                            (let [view (chart #js {:el vega-container})]
-                              (.update view))))))
+      (render-vega-visualization vega-spec
+                                 (om/get-node owner "vega-container")
+                                 responsive?))
+
     om/IRender
     (render [_]
       (html
