cf1b46881ae4fcf479d17cc92bd3ee007506f06b
Okal Otieno <okal@justokal.com>
2016-06-13 10:53:32 +0300

    OO: Bump up to v0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onaio/vega-viewer "0.5.2-SNAPSHOT"
+(defproject onaio/vega-viewer "0.6.0"
   :description "Om component that renders a vega chart from a spec"
   :url "https://github.com/onaio/vega-viewer"
   :license {:name "Eclipse Public License"
