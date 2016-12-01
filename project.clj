8858127688cd8c37a432395ac3f9f27d06d463b5
Roy Rutto <royrutto@gmail.com>
2016-12-01 09:01:54 +0300

    0.8.2 SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onaio/vega-viewer "0.8.1"
+(defproject onaio/vega-viewer "0.8.2-SNAPSHOT"
   :description "Om component that renders a vega chart from a spec"
   :url "https://github.com/onaio/vega-viewer"
   :license {:name "Eclipse Public License"
