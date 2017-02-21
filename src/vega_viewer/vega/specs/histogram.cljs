248f6fcc723bb5b729801de10e2eaa454b69347f
pld <peetldee@gmail.com>
2017-02-21 14:47:49 -0500

    only assoc titles if not null

--- src/vega_viewer/vega/specs/histogram.cljs
+++ src/vega_viewer/vega/specs/histogram.cljs
@@ -12,49 +12,47 @@
 (def histogram-spec-template
   {:data [{:name "entries"
            :values []
-           :transform [{:type "bin"
-                        :field "value"
+           :transform [{:type    "bin"
+                        :field   "value"
                         :maxbins maximum-bin-count
                         :minstep 1}]}
-          {:name "summary"
-           :source "entries"
-           :transform [{:type "aggregate"
-                        :groupby ["bin_start" "bin_end"]
+          {:name      "summary"
+           :source    "entries"
+           :transform [{:type      "aggregate"
+                        :groupby   ["bin_start" "bin_end"]
                         :summarize {:* ["count"]}}]}]
-   :scales [{:name "x"
-             :type "linear"
-             :range "width"
+   :scales [{:name   "x"
+             :type   "linear"
+             :range  "width"
              :domain {:data "summary" :field ["bin_start" "bin_end"]}}
-            {:name "y"
-             :type "linear"
+            {:name   "y"
+             :type   "linear"
              :domain {:data "summary" :field "count" :sort true}
-             :range "height"}]
-   :axes [{:type "x"
-           :scale "x"
-           :ticks default-histogram-tick-count
-           :layer "back"
-           :title nil}
-          {:type "y"
-           :scale "y"
-           :layer "back"
-           :title nil}]
-   :marks [{:type "rect"
+             :range  "height"}]
+   :axes [{:type     "x"
+           :scale    "x"
+           :ticks    default-histogram-tick-count
+           :layer    "back"}
+          {:type     "y"
+           :scale    "y"
+           :layer    "back"}]
+   :marks [{:type       "rect"
             :properties {:enter
-                         {:x {:scale "x" :field "bin_start" :offset 1}
-                          :x2 {:scale "x" :field "bin_end"}
-                          :y {:scale "y" :field "count"}
-                          :y2 {:field {:group "height"}}
+                         {:x    {:scale "x" :field "bin_start" :offset 1}
+                          :x2   {:scale "x" :field "bin_end"}
+                          :y    {:scale "y" :field "count"}
+                          :y2   {:field {:group "height"}}
                           :fill {:value bar-color}}}
-            :from {:data "summary"}}
-           {:type "text"
-            :from {:mark "rect"}
+            :from       {:data "summary"}}
+           {:type       "text"
+            :from       {:mark "rect"}
             :properties {:enter {:align {:value "center"}
-                                 :x {:field "x"}
-                                 :y {:field "y"}
-                                 :dx {:field "width" :mult 0.5}
-                                 :dy {:value -7}
-                                 :fill {:value "black"}
-                                 :text {:field "datum.count"}}}}]})
+                                 :x     {:field "x"}
+                                 :y     {:field "y"}
+                                 :dx    {:field "width" :mult 0.5}
+                                 :dy    {:value -7}
+                                 :fill  {:value "black"}
+                                 :text  {:field "datum.count"}}}}]})
 
 (defn generate-histogram-chart-vega-spec
   [{values :data :keys [height status-text width]}
@@ -63,13 +61,15 @@
              x-axis-title
              y-axis-title]}]
   (let [height (min (or height histogram-height) max-height)]
-    (-> histogram-spec-template
-        (update-x-axis-tick-labels x-axis-tick-label-format)
-        (assoc-in [:data 0 :values] (map (fn [value] {"value" value}) values))
-        (assoc-in [:axes 0 :title] x-axis-title)
-        (assoc-in [:axes 1 :title] y-axis-title)
-        (assoc :height height)
-        (assoc :width (or width
-                          (and (not responsive?)
-                               default-chart-width)))
-        (set-status-text status-text histogram-height))))
+    (cond-> histogram-spec-template
+      x-axis-tick-label-format
+      (update-x-axis-tick-labels x-axis-tick-label-format)
+      true (assoc-in [:data 0 :values]
+                     (map (fn [value] {"value" value}) values))
+      x-axis-title (assoc-in [:axes 0 :title] x-axis-title)
+      y-axis-title (assoc-in [:axes 1 :title] y-axis-title)
+      height (assoc :height height)
+      true (assoc :width (or width
+                             (and (not responsive?)
+                                  default-chart-width)))
+      status-text (set-status-text status-text histogram-height))))
